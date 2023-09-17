aaaa643793fc43c089f9e2709ce836523e20a035
GonzaloSalasVetre <gonzi.salas.vetre@gmail.com>
2023-09-17 13:03:03 -0300

    Punto 1 casi terminado

--- tp.c
+++ tp.c
@@ -3,13 +3,18 @@
 #include <ctype.h>
 #include <string.h>
 
-struct s {
+typedef struct s {
     int estadoFinal;
     int **tt;
     char *columnas;
-};
+} sistemaNumerico;
 
-typedef struct s sistemaNumerico;
+typedef struct nodo {
+    char *info;
+    struct nodo *sig;
+} nodoL;
+
+typedef nodoL* lista;
 
 const int estadoFinalDecimales = 2;
 const char columnasDeDecimales[5] = {'+', '-', '0', 'd'}; // d -> digitos diferentes a cero
@@ -38,6 +43,10 @@ const int ttHexa[5][3] = {
                         {4,4,4}
                       };
 
+
+const sistemaNumerico decimal = {estadoFinalDecimales, ttDecimales, columnasDeDecimales};
+const sistemaNumerico octal = {estadoFinalOctales, ttOctales, columnasDeOctales};
+const sistemaNumerico hexadecimal = {estadoFinalHexa, ttHexa, columnasDeHexa};
 
 // Funciones Auxiliares ----------------------------------------------------------------------------
 
@@ -68,6 +77,49 @@ int esLetraHexa(char c) {
 }
 
 
+
+void agregarPalabra(lista *listaDePalabras, char *palabra) {
+    lista aux = (nodoL *)malloc(sizeof(nodoL));
+	aux->info = palabra;
+	aux->sig = *listaDePalabras;
+	*listaDePalabras = aux;
+}
+
+void mostrar(lista listaP) {
+    nodoL * aux = listaP;
+
+	while(aux->sig != NULL){
+		printf(strcat(aux->info, "\n"));
+        aux = aux->sig;
+	}
+}
+
+
+// Esta funcion lo que hace es guardar las palabras, separadas por el signo $, en una lista.
+
+void separarPalabras(lista *listaDePalabras, char *cadena) {
+    char *palabra = (char *)malloc(sizeof (char) * 20);
+    unsigned j = 0;
+    for(unsigned i = 0; cadena[i]; i++){
+        if(cadena[i] == '$') {
+            char *aux = (char *)malloc(sizeof (char) * 20);
+            strcpy(aux, palabra);
+            agregarPalabra(&(*listaDePalabras), aux);
+            memset(palabra, 0, 20);
+            j = 0;
+        }
+        else if(i+1 == strlen(cadena)) {
+            palabra[j] = cadena[i];
+            agregarPalabra(&(*listaDePalabras), palabra);
+        }
+        else {
+            palabra[j] = cadena[i];
+            j++;
+        }
+    }
+}
+
+
 // Aca verifico si la palabra forma parte del lenguaje -----------------------------------------------
 
 int esPalabra(char *cadena, sistemaNumerico s) {
@@ -85,7 +137,6 @@ int esPalabra(char *cadena, sistemaNumerico s) {
 
     return 0;
 }
-
 
 
 // Acá verifica si los caracteres pertenecen al alfabeto del lenguaje --------------------------------
@@ -121,45 +172,60 @@ int verificaHexadecimales(char *s) {
 }
 
 
+// Verificar si las palabra pertenece a un sistema numérico
+
+void perteneceAUnSistemaNumerico(char *cadena, unsigned *i, unsigned *j, unsigned *k, unsigned *l) {
+    if(verificaDecimales(cadena) && esPalabra(cadena, decimal) || !strcmp(cadena, "0")) {
+        (*i)++;
+        return;
+    }
+    
+    if (verificaOctales(cadena) && esPalabra(cadena, octal)) {
+        (*j)++;
+        return;
+    }
+
+    if(verificaHexadecimales(cadena) && esPalabra(cadena, hexadecimal)) {
+        (*k)++;
+        return;
+    }
+    else {
+        (*l)++;
+        return;
+    }
+    
+}
+
+
+
+
 // Main -----------------------------------------------------------------------------------
 
 int main() {
 
-    sistemaNumerico decimal = {estadoFinalDecimales, ttDecimales, columnasDeDecimales};
-    sistemaNumerico octal = {estadoFinalOctales, ttOctales, columnasDeOctales};
-    sistemaNumerico hexadecimal = {estadoFinalHexa, ttHexa, columnasDeHexa};
+    unsigned cantDecimales = 0;
+    unsigned cantOctales = 0;
+    unsigned cantHexa = 0;
+    unsigned cantNoPertenece = 0;
 
-    char *palabra = "0xAB234";
-    char **listaDePalabras;
+    char *palabra = "0xAB234$1234$01234$0123$0";
+    lista listaDePalabras = (nodoL *)malloc(sizeof(nodoL));
+    listaDePalabras->info = NULL;
+    listaDePalabras->sig = NULL;
 
-    // for(unsigned i = 0; palabra[i]; ){
-    //     if(palabra[i] != '$') {
+    separarPalabras(&listaDePalabras, palabra);
 
-    //     }
-    // }
+    lista aux = listaDePalabras;
 
-    // printf("%d", decimal.tt[3][1]);
-    // return 0;
-
-    if(verificaDecimales(palabra) && esPalabra(palabra, decimal) || !strcmp(palabra, "0")) {
-        printf("siii es decimal :)");
-        return 0;
-    }
+	while(aux->sig != NULL){
+		perteneceAUnSistemaNumerico((aux->info), &cantDecimales, &cantOctales, &cantHexa, &cantNoPertenece);
+        aux = aux->sig;
+	}
     
-    if (verificaOctales(palabra) && esPalabra(palabra, octal)) {
-        printf("siii es octal :)");
-        return 0;
-    }
-
-    if(verificaHexadecimales(palabra) && esPalabra(palabra, hexadecimal)) {
-        printf("siii es hexa :)");
-        return 0;
-    }
-
-    else {
-        printf("no es palabra :(");
-        return 0;
-    }
+    printf("Cantidad de numeros decimales: %d \n", cantDecimales);
+    printf("Cantidad de numeros octales: %d \n", cantOctales);
+    printf("Cantidad de numeros hexadecimales: %d \n", cantHexa);
+    printf("Cantidad de palabras que no pertenecen al lenguaje: %d \n", cantHexa);
 
     return 0;
 
